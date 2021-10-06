ad5b16969bcdc53aabac03e42eeb0b55e4064953
Akhil Kanduri <akhil.kanduri@tu-dortmund.de>
2021-10-06 23:21:16 +0200

    Adding repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='saeli',
+      version='0.1',
+      description='Saeligram Learning Package',
+      #url='http://github.com/storborg/funniest',
+      author='Akhil Kanduri',
+      author_email='saeligram@gmail.com',
+      license='MIT',
+      packages=['saeli'],
+      zip_safe=False)
